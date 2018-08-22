f7b5fa50fe85c8b0c51689868f455b927707d606
AVLebedev <val5-94@mail.ru>
2018-08-22 22:53:00 +0300

    [Задание 1] Функционал кнопок -/+

--- src/counter.js
+++ src/counter.js
@@ -9,13 +9,9 @@ class Counter extends React.Component {
 		this.intervalId = null;
 		this.start = this.start.bind(this);
 		this.stop = this.stop.bind(this);		
-		this.reset = this.reset.bind(this);
-	}
-	tick() {
-		this.setState(prevState => {
-			time: prevState.time++
-		});
-		console.log(this.state.time);
+		this.reset = this.reset.bind(this);	
+		this.increment = this.increment.bind(this);	
+		this.decrement = this.decrement.bind(this);
 	}	
 	/*
 	componentDidMount() {
@@ -25,6 +21,12 @@ class Counter extends React.Component {
 		clearInterval(this.intervalId);
 	}
 	*/
+	tick() {
+		this.setState(prevState => {
+			time: prevState.time++
+		});
+		console.log(this.state.time);
+	}
 	start() {
 		this.intervalId = setInterval(this.tick.bind(this), 1000);
 	}
@@ -38,14 +40,24 @@ class Counter extends React.Component {
 			time: 0
 		});
 	}
+	increment() {
+		this.tick();
+	}
+	decrement() {
+		this.setState(prevState => {
+			time: prevState.time--;
+		});
+	}
 	render() {
 		return (
 			<div>
 				<div>Time: {this.state.time}</div>
 				<div style={{marginTop:'5px', display:'flex'}}>
+					<button style={{width:25, margin:'5px'}} onClick={this.decrement}>-</button>
 					<button style={{width:50, margin:'5px'}} onClick={this.start}>Start</button>
 					<button style={{width:50, margin:'5px'}} onClick={this.stop}>Stop</button>
 					<button style={{width:'50', margin:'5px'}} onClick={this.reset}>Reset</button>
+					<button style={{width:25, margin:'5px'}} onClick={this.increment}>+</button>
 				</div>
 			</div>
 		)
